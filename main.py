27cb723de0aeb20ecd7f60408e6b9a3bcdf9357a
varshahegde13 <varshahegde1999@gmail.com>
2024-11-20 11:41:36 +0530

    first

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+playing = True
+while playing:
+    choice = input('Roll the dice? (y/n): ').lower()
+
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'({die1}, {die2})')
+    elif choice == 'n':
+        print('Thanks for playing!!!')
+        break
+    else:
+        print('Invallid Choice! ')
+
